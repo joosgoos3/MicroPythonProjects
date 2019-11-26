d36a824175a4bf5959fab9f643349876538d5cd4
joosgoos3 <joostschoonhoven@live.nl>
2019-11-26 16:26:59 +0100

    IoT folder with micropy stuff and start for programming esp32

--- test.py
+++ test.py
@@ -1,3 +1,33 @@
-from numpy import matrixlib
+from machine import Pin
+import time
 
-print('allo')
+p0 = Pin(2, Pin.OUT)    # create output pin on GPIO0
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.5)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.5)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+p0.on()                 # set pin to "on" (high) level
+time.sleep(0.1)
+p0.off()                # set pin to "off" (low) level
+time.sleep(0.1)
+print('done')
